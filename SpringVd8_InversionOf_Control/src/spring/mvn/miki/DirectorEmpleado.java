273d6617b0fa579bc23780b01c7e1cd3e3839419
Miki Pardo <92552184+mikipardo@users.noreply.github.com>
2024-11-10 14:04:02 +0100

    c4

--- SpringVd8_InversionOf_Control/src/spring/mvn/miki/DirectorEmpleado.java
+++ SpringVd8_InversionOf_Control/src/spring/mvn/miki/DirectorEmpleado.java
@@ -4,7 +4,7 @@ public class DirectorEmpleado implements Empleados {
 
 	@Override
 	public String getTareas() {
-		// TODO Auto-generated method stub sazswss
+		// TODO Auto-generated method stub 
 		return "Gestiono la plantilla de la empresa como buen director";
 	}
 
